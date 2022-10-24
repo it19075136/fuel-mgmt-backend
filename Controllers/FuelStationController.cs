d9fa25e5f55237dc96e0aef3f38d754807ff3e13
pasindu <it19390260@my.sliit.lk>
2022-10-25 03:34:14 +0530

    fuelstation backend

---
+++ Controllers/FuelStationController.cs
@@ -0,0 +1,88 @@
+﻿using fuel_mgmt_backend.models.fuelStation;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace fuel_mgmt_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FuelStationController : ControllerBase
+    {
+        private readonly IFuelStationService fuelStationService;
+
+        public FuelStationController(IFuelStationService fuelStationService)
+        {
+            this.fuelStationService = fuelStationService;
+        }
+
+        // GET: api/<FuelStationController>
+        [HttpGet]
+        public ActionResult<List<FuelStation>> Get()
+        {
+            return fuelStationService.Get();
+        }
+
+        // GET: api/<FuelStationController>/email
+       /* [HttpGet("{email}")]
+        public ActionResult<List<FuelStation>> GetFuelStationsByEmail(string email)
+        {
+            return fuelStationService.GetFuelStationsByEmail(email);
+        }*/
+
+        // GET api/<FuelStationController>/5
+        [HttpGet("{id}")]
+        public ActionResult<FuelStation> Get(string id)
+        {
+            var fuelStation = fuelStationService.Get(id);
+
+            if (fuelStation == null)
+            {
+                return NotFound($"fuelStation with Id = {id} not found");
+            }
+
+            return fuelStation;
+        }
+
+        // POST api/<FuelStationController>
+        [HttpPost]
+        public ActionResult<FuelStation> Post([FromBody] FuelStation fuelStation)
+        {
+            fuelStationService.Create(fuelStation);
+
+            return CreatedAtAction(nameof(Get), new { id = fuelStation.Id }, fuelStation);
+        }
+
+        // PUT api/<FuelStationController>/5
+        [HttpPut("{id}")]
+        public ActionResult Put(string id, [FromBody] FuelStation fuelStation)
+        {
+            var existingFuelStation = fuelStationService.Get(id);
+
+            if (existingFuelStation == null)
+            {
+                return NotFound($"existingFuelStation with Id = {id} not found");
+            }
+
+            fuelStationService.Update(id, fuelStation);
+
+            return NoContent();
+        }
+
+        // DELETE api/<FuelStationController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(string id)
+        {
+            var existingFuelStation = fuelStationService.Get(id);
+
+            if (existingFuelStation == null)
+            {
+                return NotFound($"fuel Station with Id = {id} not found");
+            }
+
+            fuelStationService.Delete(id);
+
+            return Ok();
+        }
+    }
+}
---
+++ models/fuelStation/FuelStationStoreDbSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace fuel_mgmt_backend.models.fuelStation
+{
+    public class FuelStationStoreDbSettings : IFuelStationStoreDbSettings
+    {
+        public string FuelStationCollectionName { get; set; } = String.Empty;
+        public string DatabaseName { get; set; } = String.Empty;
+        public string ConnectionString { get; set; } = String.Empty;
+
+    }
+}
