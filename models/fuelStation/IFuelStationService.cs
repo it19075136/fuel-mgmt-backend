26b6a64adab13f7843325411c9b028bc9edbdbd7
pasindu <it19390260@my.sliit.lk>
2022-10-25 10:54:59 +0530

    get fuelstation by email

--- models/fuelStation/IFuelStationService.cs
+++ models/fuelStation/IFuelStationService.cs
@@ -4,7 +4,7 @@
     {
         List<FuelStation> Get();
         FuelStation Get(string id);
-        /*List<FuelStation> GetFuelStationsByEmail(string email);*/
+        List<FuelStation> GetFuelStationsByEmail(string email);
         FuelStation Create(FuelStation fuelStation);
         void Update(string id, FuelStation fuelStation);
         void Delete(string id);
